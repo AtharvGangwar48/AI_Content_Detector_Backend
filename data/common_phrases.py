88d83439479652d13ec1a40d50aab42d6025db51
Atharv Gangwar <152419242+AtharvGangwar48@users.noreply.github.com>
2025-05-28 11:37:21 +0530

    Update common_phrases.py
    some more phrases are added

--- data/common_phrases.py
+++ data/common_phrases.py
@@ -1,18 +1,48 @@
 common_phrases = [
-    "it is important to note",
-    "in conclusion",
-    "furthermore",
-    "as a result",
-    "therefore",
-    "moreover",
-    "in this essay",
-    "undoubtedly",
-    "in other words",
-    "to sum up",
-    "based on the evidence",
-    "from my perspective",
-    "clearly",
-    "in light of this",
-    "this demonstrates that",
-    "it can be seen that",
+    # Essay & Report Starters
+    "in this essay", "the purpose of this essay", "this paper aims to", "in the following paragraphs",
+    "this essay will discuss", "this report examines", "we will explore", "let us begin by",
+
+    # Conclusion/Transition
+    "in conclusion", "to sum up", "to conclude", "in summary", "to summarize",
+    "all things considered", "in closing", "in light of this", "as a final note",
+
+    # Evidence & Analysis
+    "based on the evidence", "research has shown", "data indicates", "as seen in",
+    "this demonstrates that", "this suggests that", "it can be seen that", "it is evident that",
+    "studies have revealed", "statistics show", "empirical data supports",
+
+    # Emphasis & Importance
+    "it is important to note", "it must be noted", "it is crucial to understand", "one must recognize",
+    "significantly", "notably", "clearly", "undoubtedly", "evidently", "it should be emphasized",
+
+    # Logical Flow & Transition
+    "furthermore", "moreover", "in addition", "besides that", "additionally", "on the other hand",
+    "in contrast", "conversely", "however", "nevertheless", "nonetheless", "as a result", "therefore",
+    "thus", "hence", "due to this", "consequently",
+
+    # Clarification & Restatement
+    "in other words", "to put it simply", "to clarify", "that is to say", "in simpler terms",
+    "to rephrase", "what this means is", "put differently", "in effect",
+
+    # Personal Perspective
+    "from my perspective", "I believe that", "in my opinion", "as far as I am concerned",
+    "it seems to me that", "I would argue that", "I am of the opinion that",
+
+    # Common Sentence Fillers
+    "there are many reasons why", "one might argue that", "another factor to consider is",
+    "the main point is", "this leads us to", "what this shows is that", "we can see that",
+    "let's take a closer look", "it is commonly believed that", "some people think that",
+
+    # Generalizing Statements
+    "in most cases", "generally speaking", "for the most part", "in many situations",
+    "as a general rule", "as is often the case", "more often than not",
+
+    # Conditional / Cause-effect
+    "if this is the case", "assuming that", "given that", "should this occur",
+    "in the event that", "in such a case", "this may result in",
+
+    # Summary-like Phrases
+    "to wrap things up", "in brief", "as outlined above", "as previously discussed",
+    "as mentioned earlier", "as stated above"
 ]
